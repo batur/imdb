40c701bb6147b2526b4598d390b4eb7bc4fee392
cosakin <halidbaturakkurt@gmail.com>
2021-03-02 23:20:12 +0300

    Custom 404 template done

---
+++ pages/404/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@Components/Header'
+import Footer from '@Components/Footer'
+
+export default function index() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}

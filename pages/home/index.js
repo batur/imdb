540aae078cecb1252ed3950645d24f24fe1ea692
cosakin <halidbaturakkurt@gmail.com>
2021-03-07 21:54:03 +0300

    Pages updated

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './style.module.scss'
 import Header from '@Components/Header'
 import Footer from '@Components/Footer'
+import GridList from '@Components/GridList'
 import Link from 'next/link'
+import { useDispatch, useSelector } from 'react-redux'
+import { getData } from 'src/Redux/Actions/index'
 
 function Home() {
   return (
     <>
-      <Header />
+      <Header subtitle={'Titles'} />
       <main className={styles.main}>
         <div className={styles.container}>
           <Link href="/movies">
             <a>
-              <h2>Movies</h2>
+              <h3>Movies</h3>
             </a>
           </Link>
           <Link href="/series">
             <a>
-              <h2>Series</h2>
+              <h3>Series</h3>
             </a>
           </Link>
         </div>
--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './style.module.scss'
 import Header from '@Components/Header'
 import Footer from '@Components/Footer'
 import Filter from '@Components/Filter'
 import GridList from '@Components/GridList'
+import GridItem from 'src/Elements/GridItem'
+import { useDispatch, useSelector } from 'react-redux'
+import { getData } from 'src/Redux/Actions/index'
 
-function Movies() {
+const Movies = () => {
+  const dispatch = useDispatch()
+  const { movies, isLoading, message } = useSelector((state) => state.postData)
+
+  useEffect(() => {
+    message === '' ? dispatch(getData()) : console.log('dolu')
+  }, [])
+
   return (
     <>
-      <Header />
+      <Header subtitle={'Movies'} />
       <main className={styles.main}>
         <div className={styles.container}>
-          <Filter />
-          <GridList />
+          {isLoading ? null : <Filter />}
+          <GridList>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              [
+                movies &&
+                  movies.map((item) => (
+                    <GridItem
+                      key={item.title}
+                      postersrc={item.images['Poster Art'].url}
+                      title={item.title}
+                    />
+                  )),
+              ]
+            )}
+          </GridList>
         </div>
       </main>
       <Footer />

3a44bf3fa65cf48df5c49cc3c7616d4e09bdea75
cosakin <halidbaturakkurt@gmail.com>
2021-03-09 14:55:49 +0300

    Home Page layout updated

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,28 +1,39 @@
 import React, { useEffect } from 'react'
+import Head from 'next/head'
 import styles from './style.module.scss'
 import Header from '@Components/Header'
 import Footer from '@Components/Footer'
 import GridList from '@Components/GridList'
+import GridItem from 'src/Elements/GridItem'
 import Link from 'next/link'
 import { useDispatch, useSelector } from 'react-redux'
 import { getData } from 'src/Redux/Actions/index'
 
 function Home() {
+  const pages = [
+    {
+      title: 'Movies',
+      poster: 'dummy.jpg',
+    },
+    {
+      title: 'Series',
+      poster: 'dummy.jpg',
+    },
+  ]
   return (
     <>
+      <Head>
+        <title>Popular Titles | Demo Streaming </title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <Header subtitle={'Titles'} />
       <main className={styles.main}>
         <div className={styles.container}>
-          <Link href="/movies">
-            <a>
-              <h3>Movies</h3>
-            </a>
-          </Link>
-          <Link href="/series">
-            <a>
-              <h3>Series</h3>
-            </a>
-          </Link>
+          <GridList>
+            {pages.map((item) => (
+              <GridItem key={item.title} title={item.title} postersrc={item.poster} />
+            ))}
+          </GridList>
         </div>
       </main>
       <Footer />

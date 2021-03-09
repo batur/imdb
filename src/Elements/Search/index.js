9a075a8afc2d043a3044bfbad8912895744c11d7
cosakin <halidbaturakkurt@gmail.com>
2021-03-09 11:06:43 +0300

    Component actions done

--- src/Elements/Search/index.js
+++ src/Elements/Search/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import SearchIcon from '@Elements/Icons/Search'
 import styles from './style.module.scss'
+import { connect, useDispatch } from 'react-redux'
+import { filterByTitle } from 'src/Redux/Actions'
 
 function Search() {
+  const dispatch = useDispatch()
+  const filterByValue = (e) => {
+    let input = e.target.value
+    console.log(input)
+    dispatch(filterByTitle({ value: input }))
+  }
   return (
     <div className={styles.searchBox}>
-      <form className={styles.form}>
-        <input className={styles.input} type="text" placeholder="Search..." />
-        <button type="submit" className={styles.searchbtn}>
+      <div className={styles.form}>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => {
+            filterByValue(e)
+          }}
+        />
+        <button className={styles.searchbtn}>
           <SearchIcon />
         </button>
-      </form>
+      </div>
     </div>
   )
 }
--- src/Elements/Sort/index.js
+++ src/Elements/Sort/index.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import styles from './style.module.scss'
+import { connect, useDispatch } from 'react-redux'
+import { sortByTitle, sortByYear } from 'src/Redux/Actions/index'
 
 function Sort() {
+  const dispatch = useDispatch()
+  const sortByInput = (e) => {
+    let value = e.target.value
+    console.log(value)
+    let direction = value.endsWith('Ascending') ? 'asc' : 'desc'
+    console.log(direction)
+    if (value.startsWith('Alpha')) {
+      dispatch(sortByTitle({ direction }))
+    } else {
+      dispatch(sortByYear({ direction }))
+    }
+  }
   return (
     <div className={styles.sortBox}>
-      <form className={styles.form}>
-        <select>
-          <option>Year -</option>
-          <option>Year +</option>
-          <option>Alpha +</option>
-          <option>Alpha -</option>
+      <div className={styles.form}>
+        <select
+          onChange={(e) => {
+            sortByInput(e)
+          }}
+        >
+          <option disabled selected>
+            Sort By...
+          </option>
+          <option>Year Descending</option>
+          <option>Year Ascending</option>
+          <option>Alpha Ascending</option>
+          <option>Alpha Descending</option>
         </select>
-      </form>
+      </div>
     </div>
   )
 }

938233bfc2cb9c18656c4c2a23be2ea2c47e1d85
cosakin <halidbaturakkurt@gmail.com>
2021-03-09 14:44:57 +0300

    404 page updated

--- pages/404/index.js
+++ pages/404/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import Head from 'next/head'
 import Header from '@Components/Header'
 import Footer from '@Components/Footer'
+import styles from './style.module.scss'
+import Link from 'next/link'
 
 export default function index() {
   return (
     <>
+      <Head>
+        <title>Oops | 404 </title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <Header />
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <p>Oops, something went wrong...</p>
+          <Link href={'/'}>
+            <a>
+              <h2 className={styles.homebtn}>Go Back Home</h2>
+            </a>
+          </Link>
+        </div>
+      </main>
       <Footer />
     </>
   )

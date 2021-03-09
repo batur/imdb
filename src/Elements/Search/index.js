55ec22e6982580fa4f048521e07230844cdf810f
cosakin <halidbaturakkurt@gmail.com>
2021-03-09 17:16:08 +0300

    Search element updated

--- src/Elements/Search/index.js
+++ src/Elements/Search/index.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SearchIcon from '@Elements/Icons/Search'
 import styles from './style.module.scss'
 import { connect, useDispatch } from 'react-redux'
 import { filterByTitle } from 'src/Redux/Actions'
 
 function Search() {
+  let input = ''
   const dispatch = useDispatch()
   const filterByValue = (e) => {
-    let input = e.target.value
+    input = e.target.value
     console.log(input)
     dispatch(filterByTitle({ value: input }))
   }
+  useEffect(() => {
+    dispatch(filterByTitle({ value: input }))
+  }, [])
   return (
     <div className={styles.searchBox}>
       <div className={styles.form}>

6f05384dc78bac22ffe189f579e6dfe4e6d20294
cosakin <halidbaturakkurt@gmail.com>
2021-04-02 16:09:52 +0300

    404 page updated

--- pages/404/index.js
+++ pages/404/index.js
@@ -12,18 +12,12 @@ export default function index() {
         <title>Oops | 404 </title>
         <meta property="og:title" content="My page title" key="title" />
       </Head>
-      <Header />
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <p>Oops, something went wrong...</p>
-          <Link href={'/'}>
-            <a>
-              <h2 className={styles.homebtn}>◀️ Go Back Home</h2>
-            </a>
-          </Link>
-        </div>
-      </main>
-      <Footer />
+      <p>Oops, something went wrong...</p>
+      <Link href={'/'}>
+        <a>
+          <h2 className={styles.homebtn}>◀️ Go Back Home</h2>
+        </a>
+      </Link>
     </>
   )
 }
